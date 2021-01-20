8ad2c3cc2a2a5cbd878aca712da74f7f66999a8f
Devke <chriseke13@gm>
2021-01-20 17:13:28 -0600

    Added a object containing a list of like movies

--- index.js
+++ index.js
@@ -3,8 +3,62 @@ const morgan = require('morgan');
 
 const app = express();
 
+// Movies List
+let movies = [
+    {
+        title: "Ocean's Eleven",
+        genres: ["Thriller", "Comedy", "Crime"],
+        director: "Steven Soderbergh"
+    },
+    {
+        title: "Ocean's Twelve",
+        genres: ["Thriller", "Comedy", "Crime"],
+        director: "Steven Soderbergh"
+    },
+    {
+        title: "Guardians of the Galaxy",
+        genres: ["Action", "Adventure", "Comedy"],
+        director: "James Gunn"
+    },
+    {
+        title: "Friday",
+        genres: ["Comedy", "Drama"],
+        director: "F. Gary Gary"
+    },
+    {
+        title: "Inception",
+        genres: ["Action", "Adventure", "Sci-Fi"],
+        director: "Christopher Nolan"
+    },
+    {
+        title: "Rush Hour",
+        genres: ["Action", "Comedy", "Crime"],
+        director: "Brett Ratner"
+    },
+    {
+        title: "V for Vendetta",
+        genres: ["Action", "Drama", "Sci-Fi"],
+        director: "James McTeigue"
+    },
+    {
+        title: "28 Weeks Later",
+        genres: ["Action", "Horror", "Sci-Fi"],
+        director: "Juan Carlos Fresnadillo"
+    },
+    {
+        title: "Avengers: Endgame",
+        genres: ["Action", "Adventure", "Drama"],
+        director: "Anthony Russo"
+    },
+    {
+        title: "SAW",
+        genres: ["Horror", "Mystery", "Crime"],
+        director: "James Wan"
+    }
+]
+
+
 let errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
     res.status(500).send("Sorry, Something went wrong!");
 }
 
@@ -15,7 +69,7 @@ app.use(errorHandler());
 
 // GET requests
 app.get('/movies', (req, res) => {
-    res.json(data);
+    res.json(movies);
 });
 
 app.get('/', (req, res) => {
