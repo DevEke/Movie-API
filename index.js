979b5fc7687dc5865b961ffb7ef55c442a465ec2
Devke <chriseke13@gm>
2021-01-20 17:15:36 -0600

    Removed variable for error handler to fix a coding issue

--- index.js
+++ index.js
@@ -59,13 +59,17 @@ let movies = [
 
 
 let errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
     res.status(500).send("Sorry, Something went wrong!");
 }
 
 // Middleware
 app.use(express.static('public'));
 app.use(morgan('common'));
-app.use(errorHandler());
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Sorry, Something went wrong!");
+});
 
 // GET requests
 app.get('/movies', (req, res) => {
