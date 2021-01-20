8c2520cc8377511e6ef185e29a44f9079318f22e
Devke <chriseke13@gm>
2021-01-20 16:17:41 -0600

    Added error handler middleware

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const morgan = require('morgan');
 
 const app = express();
 
+let errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Sorry, Something went wrong!");
+}
 
+// Middleware
 app.use(express.static('public'));
 app.use(morgan('common'));
+app.use(errorHandler());
 
 // GET requests
 app.get('/movies', (req, res) => {
