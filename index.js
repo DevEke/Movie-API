9cbef48edd34a62d1d3ab4728c6098f83cf92408
Devke <chriseke13@gm>
2021-01-26 07:37:41 -0600

    Removed authentication for registering new users

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.get('/directors/:Name',passport.authenticate('jwt', {session: false}), (req,
 
 
 // POST Requests
-app.post('/users',passport.authenticate('jwt', {session: false}), (req, res) => {
+app.post('/users', (req, res) => {
     Users.findOne({ Username: req.body.Username })
         .then((user) => {
             if (user) {
