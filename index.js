6e8e4edf1424ca8292dd5cf3bfba215811d032d8
Devke <chriseke13@gm>
2021-01-26 09:03:21 -0600

    Updated listening port

--- index.js
+++ index.js
@@ -209,6 +209,7 @@ app.delete('/users/:Username',passport.authenticate('jwt', {session: false}), (r
 
 
 // Listening Port
-app.listen(8080, () => {
-    console.log('Listening on port 8080');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+    console.log('Listening on port ' + port);
 });
