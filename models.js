03d36ba8cf9e369ef0e1f5eecf5fb62c660e9140
Devke <chriseke13@gm>
2021-01-26 10:55:58 -0600

    updated hashPassword code

--- models.js
+++ models.js
@@ -26,7 +26,7 @@ let userSchema = mongoose.Schema({
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
-userSchema.statics.hashPassowrd = (password) => {
+userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
 }
 
