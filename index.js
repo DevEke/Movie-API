978106e5bc077c9a34234a3dde6b2cfe810e0ce2
Devke <chriseke13@gm>
2021-01-27 08:45:42 -0600

    Temporarily removed authentication from movies endpoint for testing

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
     res.status(200).send(message);
 })
 
-app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(200).json(movies);
